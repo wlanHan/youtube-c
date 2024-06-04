5634d14144276068a1d3a1e09df3f754c0973bb0
Efe Han Oz <hanstudiocontact@gmail.com>
2024-06-04 14:17:27 +0300

    navbar +

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+const Home = () => {
+    return (
+        <>
+            <Sidebar />
+        </>
+    )
+}
+
+export default Home

98059cead9cd062ff0cdeb205b5c9493caf63843
Efe Han Oz <hanstudiocontact@gmail.com>
2024-06-05 17:03:16 +0300

    Recomended api data +

--- src/Components/Recomended/Recomended.jsx
+++ src/Components/Recomended/Recomended.jsx
@@ -1,14 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './Recomended.css'
-import thumbnail1 from '../../assets/thumbnail1.png'
-import thumbnail2 from '../../assets/thumbnail2.png'
-import thumbnail3 from '../../assets/thumbnail3.png'
-import thumbnail4 from '../../assets/thumbnail4.png'
-import thumbnail5 from '../../assets/thumbnail5.png'
-import thumbnail6 from '../../assets/thumbnail6.png'
-import thumbnail7 from '../../assets/thumbnail7.png'
-import thumbnail8 from '../../assets/thumbnail8.png'
-import { API_KEY } from '../../data'
+import { API_KEY, value_converter } from '../../data'
 
 
 const Recomended = ({ categoryId }) => {
@@ -16,7 +8,7 @@ const Recomended = ({ categoryId }) => {
     const [apiData, setApiData] = useState([]);
 
     const fetchData = async () => {
-        const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`
+        const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`
         await fetch(relatedVideo_url)
             .then(res => res.json())
             .then(data => setApiData(data.items))
@@ -28,70 +20,19 @@ const Recomended = ({ categoryId }) => {
 
     return (
         <div className='recomended' >
-            <div className="side-video-list">
-                <img src={thumbnail1} alt="thumbnail" />
-                <div className="vid-info">
-                    <h4>Lorem ipsum dolor sit amet  consectetur consectetur.</h4>
-                    <p>wlanHan</p>
-                    <p>132K Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src={thumbnail2} alt="thumbnail" />
-                <div className="vid-info">
-                    <h4>Lorem ipsum dolor sit amet  consectetur consectetur.</h4>
-                    <p>wlanHan</p>
-                    <p>132K Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src={thumbnail3} alt="thumbnail" />
-                <div className="vid-info">
-                    <h4>Lorem ipsum dolor sit amet  consectetur consectetur.</h4>
-                    <p>wlanHan</p>
-                    <p>132K Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src={thumbnail4} alt="thumbnail" />
-                <div className="vid-info">
-                    <h4>Lorem ipsum dolor sit amet  consectetur consectetur.</h4>
-                    <p>wlanHan</p>
-                    <p>132K Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src={thumbnail5} alt="thumbnail" />
-                <div className="vid-info">
-                    <h4>Lorem ipsum dolor sit amet  consectetur consectetur.</h4>
-                    <p>wlanHan</p>
-                    <p>132K Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src={thumbnail6} alt="thumbnail" />
-                <div className="vid-info">
-                    <h4>Lorem ipsum dolor sit amet  consectetur consectetur.</h4>
-                    <p>wlanHan</p>
-                    <p>132K Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src={thumbnail7} alt="thumbnail" />
-                <div className="vid-info">
-                    <h4>Lorem ipsum dolor sit amet  consectetur consectetur.</h4>
-                    <p>wlanHan</p>
-                    <p>132K Views</p>
-                </div>
-            </div>
-            <div className="side-video-list">
-                <img src={thumbnail8} alt="thumbnail" />
-                <div className="vid-info">
-                    <h4>Lorem ipsum dolor sit amet  consectetur consectetur.</h4>
-                    <p>wlanHan</p>
-                    <p>132K Views</p>
-                </div>
-            </div>
+            {apiData.map((item, i) => {
+                return (
+                    <div key={i} className="side-video-list">
+                        <img src={item.snippet.thumbnails.medium.url} alt="thumbnail" />
+                        <div className="vid-info">
+                            <h4>{item.snippet.title}</h4>
+                            <p>{item.snippet.channelTitle}</p>
+                            <p>{value_converter(item.statistics.viewCount)} Görüntülenme</p>
+                        </div>
+                    </div>
+                )
+            })}
+
         </div>
     )
 }

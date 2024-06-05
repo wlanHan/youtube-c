e20e9387a34e087a2895219c9521be7e1d93ba60
Efe Han Oz <hanstudiocontact@gmail.com>
2024-06-05 15:24:21 +0300

    views and upload infos +

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './PlayVideo.css'
 import video1 from '../../assets/video.mp4'
 import like from '../../assets/like.png'
@@ -7,14 +7,34 @@ import share from '../../assets/share.png'
 import save from '../../assets/save.png'
 import jack from '../../assets/jack.png'
 import user_profile from '../../assets/user_profile.jpg'
+import { API_KEY, value_converter } from '../../data'
+import moment from 'moment'
 
-const PlayVideo = () => {
+const PlayVideo = ({ videoId }) => {
+
+    const [apiData, setApiData] = useState(null);
+
+    const fetchVideoData = async () => {
+        //Fetching videos data
+        const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`
+        await fetch(videoDetails_url)
+            .then(res => res.json())
+            .then(data => setApiData(data.items[0]))
+    }
+
+    useEffect(() => {
+        fetchVideoData()
+    }, [])
+
     return (
         <div className='play-video'>
-            <video src={video1} controls autoPlay muted></video>
-            <h3>Best youtube channel to learn web development</h3>
+            {/* <video src={video1} controls autoPlay muted></video> */}
+
+            <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+            <h3>{apiData ? apiData.snippet.title : 'Title Here'}</h3>
             <div className="play-video-info">
-                <p>1795 Views &bull; 2 days ago</p>
+                <p>{apiData ? value_converter(apiData.statistics.viewCount) : 'Viewers Here'} &bull; {apiData ? moment(apiData.snippet.publishedAt).fromNow(): ''}</p>
                 <div>
                     <span><img src={like} alt="like" />884</span>
                     <span><img src={dislike} alt="like" />254</span>

632d084a5e0f78ebaccc66a0241af77bd42c65b3
Efe Han Oz <hanstudiocontact@gmail.com>
2024-06-05 17:06:36 +0300

    bugfix +

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -9,9 +9,11 @@ import jack from '../../assets/jack.png'
 import user_profile from '../../assets/user_profile.jpg'
 import { API_KEY, value_converter } from '../../data'
 import moment from 'moment'
-import { useFetcher } from 'react-router-dom'
+import { useFetcher, useParams } from 'react-router-dom'
 
-const PlayVideo = ({ videoId }) => {
+const PlayVideo = () => {
+
+    const { videoId } = useParams()
 
     const [apiData, setApiData] = useState(null);
     const [channelData, setChannelData] = useState(null)
@@ -41,7 +43,7 @@ const PlayVideo = ({ videoId }) => {
 
     useEffect(() => {
         fetchVideoData()
-    }, [])
+    }, [videoId])
 
     useEffect(() => {
         fetchOtherData();
--- src/Components/Recomended/Recomended.jsx
+++ src/Components/Recomended/Recomended.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Recomended.css'
 import { API_KEY, value_converter } from '../../data'
+import { Link } from 'react-router-dom';
 
 
 const Recomended = ({ categoryId }) => {
@@ -22,14 +23,14 @@ const Recomended = ({ categoryId }) => {
         <div className='recomended' >
             {apiData.map((item, i) => {
                 return (
-                    <div key={i} className="side-video-list">
+                    <Link to={`/video/${item.snippet.categoryId}/${item.id}`} key={i} className="side-video-list">
                         <img src={item.snippet.thumbnails.medium.url} alt="thumbnail" />
                         <div className="vid-info">
                             <h4>{item.snippet.title}</h4>
                             <p>{item.snippet.channelTitle}</p>
                             <p>{value_converter(item.statistics.viewCount)} Görüntülenme</p>
                         </div>
-                    </div>
+                    </Link>
                 )
             })}
 
